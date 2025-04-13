307793558fb9f51b072255a66774a5b70a537be6
senzomaxwell1@gmail.com <senzomaxwell1@gmail.com>
2025-04-13 14:53:03 +0200

    initial commit

--- wellness_project/wellness_app/urls.py
+++ wellness_project/wellness_app/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
-from .views import home_view, task_create_view, wellness_check_view
+from django.urls import path # type: ignore
+from .views import home_view, task_create_view, wellness_check_view, login_view, resources_view, campus_view, register_view, schedule_view, about_view
+
 
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path('', login_view , name='login' ),
+    path('register', register_view , name='register' ),
+    path('home', home_view, name='home'),
     path('task_create/', task_create_view, name='task_create'), 
     path('wellness/check-in/', wellness_check_view, name='wellness_check'),
+    path('view_schedule', schedule_view , name='view_schedule' ),
+    path('resources/', resources_view, name='resources'), 
+    path('campus-life/', campus_view, name='campus-life'), 
+    path('about/', about_view, name='about'), 
+
+    
 ]
--- wellness_project/wellness_app/views.py
+++ wellness_project/wellness_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render # type: ignore
 
 # Create your views here.
 def home_view(request):
@@ -15,3 +15,39 @@ def wellness_check_view(request):
         # Process wellness data
         return redirect('home')
     return render(request, 'wellness_check.html')
+
+def schedule_view(request):
+    if request.method == 'POST':
+        # Process wellness data
+        return redirect('home')
+    return render(request, 'view_schedule.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        # Process wellness data
+        return redirect('home')
+    return render(request, 'login.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        # Process wellness data
+        return redirect('home')
+    return render(request, 'register.html')
+
+def resources_view(request):
+    if request.method == 'POST':
+        # Process wellness data
+        return redirect('home')
+    return render(request, 'resources.html')
+
+def campus_view(request):
+    if request.method == 'POST':
+        # Process wellness data
+        return redirect('home')
+    return render(request, 'campus_life.html')
+
+def about_view(request):
+    if request.method == 'POST':
+        # Process wellness data
+        return redirect('home')
+    return render(request, 'about.html')
